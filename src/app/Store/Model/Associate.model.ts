6373d2866a2adf3edde64fc15db734c7ec377eba
Aravind <aravindmunna240@gmail.com>
2023-11-25 11:18:45 +0530

    folder management

---
+++ src/app/Store/Model/Associate.model.ts
@@ -0,0 +1,11 @@
+export interface Associates{
+    id:number,
+    name:string,
+    email:string,
+    phone:number,
+    type:string,
+    address:string,
+    associateGroup:string,
+    status:boolean
+
+}
---
+++ src/app/component/associatelisting/associatelisting.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-associatelisting',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './associatelisting.component.html',
+  styleUrl: './associatelisting.component.css'
+})
+export class AssociatelistingComponent {
+
+}
