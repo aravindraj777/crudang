1302956e0525f6a751b1bc0a90d445355544aa1d
Aravind <aravindmunna240@gmail.com>
2023-11-25 18:24:38 +0530

    addform done

--- src/app/component/associatelisting/associatelisting.component.ts
+++ src/app/component/associatelisting/associatelisting.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../../Material.module';
+import { MatDialog } from '@angular/material/dialog';
+import { AddassociateComponent } from '../addassociate/addassociate.component';
 
 @Component({
   selector: 'app-associatelisting',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,MaterialModule],
   templateUrl: './associatelisting.component.html',
   styleUrl: './associatelisting.component.css'
 })
-export class AssociatelistingComponent {
+export class AssociatelistingComponent implements OnInit {
+  
+  constructor(private dialog:MatDialog){}
+  ngOnInit(): void {
+    
+  }
 
+  functionAdd(){
+    this.openPopUp(0,'Create Assosciate ')
+  }
+
+  openPopUp(code:number,title:string){
+    this.dialog.open(AddassociateComponent,{
+      width:'50%',
+      enterAnimationDuration:'1000ms',
+      exitAnimationDuration:'1000ms',
+      data:{
+        code:code,
+        title:title
+      }
+    })
+  }
 }
